9d38aa247628bca1be028c4ccea7a8d2654a06ef
vinaykale64 <vinaykale64@gmail.com>
2020-11-25 13:43:23 -0500

    added options for classical music

--- README.md
+++ README.md
@@ -6,16 +6,25 @@
 ![pypi](https://img.shields.io/badge/pypi-v0.0.2-blue)
 ![license](https://img.shields.io/badge/license-MIT-white)
 
-Python package to play music in background while your code runs.
+Have you ever got bored of running long chunks of code? 
+
+It can get repetitive to monitor the progress of the code continuously.
+
+Euphony can help you. It is a Python package that plays classical 
+music in background while your code runs whether it be on terminal 
+or script or jupyter notebook. Once your code is complete
+or if it errors out, it stops so you can go back to it. 
 
 ## How to use
 
-Its pretty simple to use. Create a instance of the `player` class and let your code run enclosed in a with statement with the instance.
+Its pretty simple to use. Create a instance of the `player` class and let your code run enclosed 
+in a `with` statement with the object. You can choose between `bach`, `beethoven`
+or `mozart`. If not specified, it chooses at random. 
 
 ``` python
 
   from euphony import player
-  mozart = player.player()
+  mozart = player.player(artist = 'mozart') # options: ['bach', 'beethoven']
 
   with mozart:
       for i in range(1000000000):
--- euphony/player.py
+++ euphony/player.py
@@ -1,19 +1,72 @@
 import os
+import random
 import pkg_resources
-
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 from pygame import mixer
 
+
 class player:
-    def __init__(self):
-        pass
+    """
+    The player class creates an instance which can be used to play classical
+    music while code runs in background. It's instance will be used as an
+    argument to the `with` statement.
+
+    Parameters
+    ----------
+
+    artist: str, Optional
+        Choose among 'bach', 'beethoven' and 'mozart' to play specific music. If
+        not specified, the code plays a random music file.
+
+    Returns
+    -------
+    object : type player
+        To be used as an argument for `with` statement to wrap code around
+
+    Example
+    -------
+
+    .. code-block:: python
+
+        from euphony import player
+        random = player.player() # random music selector
+        bach = player.player(artist='bach') # for bach music
+        beethoven = player.player(artist='beethoven') # for beethoven music
+        mozart = player.player(artist='mozart') # for mozart music
+
+        # Works for any of the above 4
+        with mozart:
+            for i in range(1000000000):
+                pass
+        # Music plays till code completes and then stops.
+        ...
+
+    """
+    def __init__(self, artist=None):
+        self.artist = artist
 
     def __enter__(self):
         mixer.init()
-        path = 'music_templates/sample.mp3'
+        templates = os.listdir(
+            pkg_resources.resource_filename(__name__, "music_templates")
+        )
+        if ".DS_Store" in templates:
+            templates.remove(".DS_Store")
+
+        if self.artist == "beethoven":
+            templates = [template for template in templates if "beethoven" in template]
+        elif self.artist == "bach":
+            templates = [template for template in templates if "bach" in template]
+        elif self.artist == "mozart":
+            templates = [template for template in templates if "mozart" in template]
+        else:
+            pass
+
+        song = random.choice(templates)
+        path = "music_templates/" + song
         filepath = pkg_resources.resource_filename(__name__, path)
         mixer.music.load(filepath)
-        mixer.music.play()
+        mixer.music.play(loops=-1)
 
     def __exit__(self, exc_type, exc_value, tb):
         if exc_type is not None:
