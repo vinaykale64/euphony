0c80ddc04f8f8f30324f8c7413a223c8ccbd44fb
vinaykale64 <vinaykale64@gmail.com>
2020-11-25 08:58:44 -0500

    revert version to 0.1.1.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(HERE, "README.md")) as fid:
 # This call to setup() does all the work
 setup(
     name="acoustic",
-    version="1.0.0",
+    version="0.0.1",
     description="Play music while your code runs",
     long_description=README,
     long_description_content_type="text/markdown",
